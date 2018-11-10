c54f2870d623dfdc3b67e735c879aba2c3c30ac9
Hugo Striedinger <striedinger@outlook.com>
2018-11-10 16:22:54 -0500

    Initial Commit

---
+++ darkk/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  darkk
+//
+//  Created by Hugo Striedinger on 11/10/18.
+//  Copyright © 2018 Hugo Striedinger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ darkk/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  darkk
+//
+//  Created by Hugo Striedinger on 11/10/18.
+//  Copyright © 2018 Hugo Striedinger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
